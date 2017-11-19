f057fc498e03e9fdd7a8517b604c70538bfdb3f2
Timothy-2017 <timothy.eubank@gmail.com>
2017-11-19 09:23:00 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
-// Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// const title = React.createElement('h1', {}, 'My First React Code');
+// const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!');
+// const container = React.createElement('div', {}, [title, paragraph]);
+//
+// ReactDOM.render(
+//   container,
+//   document.getElementById('global')
+// );
+
+// const list =
+//   React.createElement('div', {},
+//     React.createElement('h1', {}, 'My favorite ice cream flavors'),
+//     React.createElement('ul', {},
+//       [
+//         React.createElement('li', {}, 'Chocolate'),
+//         React.createElement('li', {}, 'Vanilla'),
+//         React.createElement('li', {}, 'Banana')
+//       ]
+//     )
+//   );
+//
+// ReactDOM.render(
+//   list,
+//   document.getElementById('global')
+// );
+
+// const list =
+//   React.createElement('div', {},
+//     React.createElement('h1', {}, 'My favorite ice cream flavors'),
+//     React.createElement('ul', {},
+//       [
+//         React.createElement('li', { class: 'brown' }, 'Chocolate'),
+//         React.createElement('li', { class: 'white' }, 'Vanilla'),
+//         React.createElement('li', { class: 'yellow' }, 'Banana')
+//       ]
+//     )
+//   );
+//
+// ReactDOM.render(
+//   list,
+//   document.getElementById('global')
+// );
+
+const list =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'My favorite ice cream flavors'),
+    React.createElement('ul', {},
+      [
+        React.createElement('li', { className: 'brown' }, 'Chocolate'),
+        React.createElement('li', { className: 'white' }, 'Vanilla'),
+        React.createElement('li', { className: 'yellow' }, 'Banana')
+      ]
+    )
+  );
+
+ReactDOM.render(
+  list,
+  document.getElementById('global')
+);
